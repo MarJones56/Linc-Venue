5e3d23041f2e9b5526b397ced1e4c588e533c3da
Ben Bamidele <bbamidel@iu.edu>
2023-10-11 14:41:19 -0400

    Ben and Marcus Organized/Database

---
+++ Project/server/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+    role: String,
+    
+})
+
+const UserModel = mongoose.model("userlogin",UserSchema)
+module.exports = UserModel
